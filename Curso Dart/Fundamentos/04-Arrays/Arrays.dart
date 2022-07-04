90c10832628cdf570c61dc26d3027e12ea63ebb8
Vini0108 <58220467+Vini0108@users.noreply.github.com>
2022-07-04 15:14:48 -0700

    Parte 4 do curso concluída com sucesso.
    Parte 4 basicamente fala sobre arrays e suas funções principais. Bem simples já que vi tudo isso em JS e diversas outras linguagens.

---
+++ Curso Dart/Fundamentos/04-Arrays/Arrays.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("04.1) Arrays");
+  print("");
+
+  /* Mesmo padrão, array é um elemento ordenado e dinâmico. */
+
+  var array = [1, 2, 3, 4, 5]; // o indice começa no 0 como sempre.
+  print(array[1]);
+}
---
+++ Curso Dart/Fundamentos/04-Arrays/FunçõesArrays.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("04.2) Funções de Arrays");
+  print("");
+
+  var array = [1];
+  array.add(2); // colocar o elemento ao final do array
+  array.insert(1, 2); // colocar o elemento em um index específico nesse caso 1
+  array.removeAt(1); // apaga no index selecionado no parâmentro
+
+  /* Tem diversos outros super úteis e padrões que podem ser usados. 
+  Não os colocarei aqui pois não vejo a necessidade de colocar tantos. */
+}
